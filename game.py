92c1722b41bfc560d5739f5a9b00cb95dedab1fe
cmehadji27 <cmehadji27@gmail.com>
2024-12-07 15:15:06 +0100

    qtable

--- game.py
+++ game.py
@@ -54,13 +54,42 @@ def draw_track():
         color = (255, 0, 0) if i % 2 == 0 else (255, 255, 255)
         pygame.draw.rect(window, color, (start_line_x, 100 + i * 10, 20, 10))
 
+class QTable():
+    def __init__(self, learning_rate=1.0, discount_factor=1.0):
+        self.qtable = {}
+        self.learning_rate = learning_rate
+        self.discount_factor = discount_factor
+
+    def set(self, position, action, reward, new_position):
+        if position not in self.qtable:
+            self.qtable[position] = {pygame.K_UP: 0, pygame.K_DOWN: 0,
+                                     pygame.K_LEFT: 0, pygame.K_RIGHT: 0}
+        if new_position not in self.qtable:
+            self.qtable[new_position] = {pygame.K_UP: 0, pygame.K_DOWN: 0,
+                                         pygame.K_LEFT: 0, pygame.K_RIGHT: 0}
+        change = reward + self.discount_factor * max(self.qtable[new_position].values()) - self.qtable[position][action]
+        self.qtable[position][action] += self.learning_rate * change
+
+    def __repr__(self):
+        res = ' ' * 15
+        res += 'UP     DOWN     LEFT     RIGHT\r\n'
+        for state in self.qtable:
+            res += f'{state} '
+            for action in self.qtable[state]:
+                res += f'{self.qtable[state][action]:8.1f} '
+            res += '\r\n'
+        return res
 
 # Jeu principal
 class Game:
     def run(self):
         car = Voiture()  # Initialisation de la voiture
+        qtable = QTable()  # Création de la table Q
         running = True
         clock = pygame.time.Clock()
+        
+        # Variables pour stocker l'état précédent
+        prev_position = (int(car.car_x), int(car.car_y), int(car.car_angle))
 
         while running:
             for event in pygame.event.get():
@@ -69,12 +98,20 @@ class Game:
                     pygame.quit()
                     sys.exit()
 
+            # Position actuelle (état)
+            position = (int(car.car_x), int(car.car_y), int(car.car_angle))
+
             # Gestion des touches
             keys = pygame.key.get_pressed()
+            reward = Reward.DEFAULT.value
+            action = None
+
             if keys[pygame.K_UP]:  # avancer
                 car.car_speed = min(car.car_speed + acceleration, max_speed)
+                action = pygame.K_UP
             elif keys[pygame.K_DOWN]:  # reculer
                 car.car_speed = max(car.car_speed - acceleration, -max_speed / 2)
+                action = pygame.K_DOWN
             else:
                 # Ralenti automatique
                 if car.car_speed > 0:
@@ -84,11 +121,30 @@ class Game:
 
             if keys[pygame.K_LEFT]:  # tourner à gauche
                 car.car_angle += turning_speed
+                action = pygame.K_LEFT
             if keys[pygame.K_RIGHT]:  # tourner à droite
                 car.car_angle -= turning_speed
+                action = pygame.K_RIGHT
 
             # Mise à jour de la position de la voiture
             car.update_position()
+
+            # Nouvelle position (état futur)
+            new_position = (int(car.car_x), int(car.car_y), int(car.car_angle))
+
+            # Vérification des collisions ou hors-piste
+            if car.car_x < 100 or car.car_x > WIDTH - 100 or car.car_y < 100 or car.car_y > HEIGHT - 100:
+                reward = Reward.WALL.value
+                car.car_speed = 0  # Arrêt de la voiture
+
+            # Mise à jour de la table Q
+            if action:
+                qtable.set(position, action, reward, new_position)
+
+            # Afficher la table Q seulement si la voiture bouge
+            if position != prev_position:  # Si la position a changé
+                print(qtable)
+                prev_position = position  # Mettre à jour l'état précédent
 
             # Effacer l'écran (pelouse verte)
             window.fill((0, 150, 0))
@@ -105,7 +161,6 @@ class Game:
             pygame.display.flip()
             clock.tick(60)  # 60 FPS
 
-# Exécution du jeu
 if __name__ == "__main__":
     game = Game()
     game.run()
